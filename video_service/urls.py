0ec9f2e43c3dae1e0bfcc664d0c99533b42fe9a4
bykov <bykov@indin.ru>
2020-05-07 23:38:31 +0300

    init

--- video_service/urls.py
+++ video_service/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from video_service.views import index
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    path('', index),
     path('admin/', admin.site.urls),
 ]
